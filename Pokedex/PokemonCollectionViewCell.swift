24d7781214575dd9479f381150f8b0b54bba22ba
David Lee <david@davidlee.io>
2016-07-27 16:25:38 -0700

    added a csv parser to parse pokemon csv data and populate view cells

---
+++ Pokedex/PokemonCollectionViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  PokemonCollectionViewCell.swift
+//  Pokedex
+//
+//  Created by David Lee on 7/27/16.
+//  Copyright © 2016 David Lee. All rights reserved.
+//
+
+import UIKit
+
+class PokemonCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
